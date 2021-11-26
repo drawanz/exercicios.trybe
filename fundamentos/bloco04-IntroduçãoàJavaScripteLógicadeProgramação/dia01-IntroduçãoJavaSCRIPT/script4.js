35053a95e472f439fc1056324eec97e883068272
Leonidas <leonidas.drawanz@gmail.com>
2021-11-26 18:32:40 -0300

    exercícios do dia 01 do bloco 04

---
+++ fundamentos/bloco04-IntroduçãoàJavaScripteLógicadeProgramação/dia01-IntroduçãoJavaSCRIPT/script4.js
@@ -0,0 +1,9 @@
+
+let weekDays = "pão";
+
+if ( weekDays === "segunda" || weekDays === "terça" || weekDays === "quarta" || weekDays === "quinta" || weekDays === "sexta" ){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else{
+    console.log("FINALMENTE, descanso merecido UwU");
+}
---
+++ fundamentos/bloco04-IntroduçãoàJavaScripteLógicadeProgramação/dia01-IntroduçãoJavaSCRIPT/script5.js
@@ -0,0 +1,19 @@
+
+let estado = "pé";
+
+switch (estado) {
+    case "aprovada":
+        console.log("Aprovada!");
+        break;
+
+    case "reprovada":
+        console.log("Reprovada!!");
+        break;
+    
+    case "lista":
+        console.log("Lista!");
+        break;
+        
+    default:
+        console.log("Não se aplica.");
+}
