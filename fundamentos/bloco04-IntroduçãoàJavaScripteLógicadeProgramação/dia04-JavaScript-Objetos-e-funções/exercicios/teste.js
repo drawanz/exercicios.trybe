9f1c52cf58f57166dbd1bc566428aa7c6b7b3274
Leonidas <leonidas.drawanz@gmail.com>
2021-12-02 15:00:26 -0300

    realizado dois exercícios de função

---
+++ fundamentos/bloco04-IntroduçãoàJavaScripteLógicadeProgramação/dia04-JavaScript-Objetos-e-funções/exercicios/teste.js
@@ -0,0 +1,7 @@
+let fruits = ['Strawberry', 'Banana', 'Mango']
+fruits.push('Mango')
+// ["Strawberry", "Banana", "Mango"]
+
+let pos = fruits.indexOf('Banana')
+// 1
+console.log(fruits.indexOf('Banana'));
