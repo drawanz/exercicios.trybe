b6b7f6a1b85c71f29f3ecf8e98776cf2a3bc5c70
Leonidas <leonidas.drawanz@gmail.com>
2022-01-16 19:24:59 -0300

    exercicios do dia

---
+++ fundamentos/bloco07-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia03-primeiros-passos-em-Jest/exercicios/functions.js
@@ -0,0 +1,76 @@
+/*function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+module.exports = sum;*/
+
+/*function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+module.exports = myRemove;
+*/
+
+/*function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+
+module.exports = myFizzBuzz;*/
+
+function mapString(objectMap, string) {
+  const splitString = string.split('');
+  const mappedArray = [];
+
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+
+  return mappedArray.join('');
+}
+
+function encode(string) {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+}
+
+function decode(string) {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+}
+
+const functions = { encode, decode };
+module.exports = functions;
---
+++ fundamentos/bloco07-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia03-primeiros-passos-em-Jest/exercicios/functions.test.js
@@ -0,0 +1,36 @@
+/*const sum = require('./sum.js');
+
+test('sums 4 and 5 to be equal to 9', () => {
+    expect(sum(4, 5)).toBe(9);
+});
+
+test('sums 0 and 0 and expect to be 0', () => {
+    expect(sum(0, 0)).toBe(0);
+})*/
+
+/*const myRemove = require('./myRemove.js')
+test('introduce the parameters ([1, 2, 3, 4], 3) and expected to be [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+})
+test('introduce the parameters ([1, 2, 3, 4], 3) and expected not to be [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+})
+test('introduce the parameters ([1, 2, 3, 4], 5) and expected to be [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+})*/
+
+/*const myFizzBuzz = require('./myFizzBuzz')
+test('receive 15 and try to divided by 3 and 5, the return must be fizzbuzz', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+})
+test('receive 3 and try to divided by 3 and 5, the return must be fizz', () => {
+    expect(myFizzBuzz(3)).toBe('fizz');
+})
+test('receive 5 and try to divided by 3 and 5, the return must be buzz', () => {
+    expect(myFizzBuzz(5)).toBe('buzz');
+})
+test('receive 7 and try to divided by 3 and 5, the return must be buzz', () =>{
+    expect(myFizzBuzz(7)).toBe(7);
+})*/
+
+const functions = require('./functions')
